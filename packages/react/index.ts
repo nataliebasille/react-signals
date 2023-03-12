a03efbc27cae5305c45c6fbaad8a6b6cda08c148
Natalie Basille <natalie.basille1@gmail.com>
2023-03-12 15:58:06 -0400

    feat: reexport all of core from signals-react

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,3 +2,4 @@ export * from './src/SignalNode';
 export * from './src/useComputedSignal';
 export * from './src/useSignal';
 export * from './src/useSignalEffect';
+export * from '@nataliebasille/signals-core';
